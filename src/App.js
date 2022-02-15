e8a36040d27cf19bae16535b46678a9b320493af
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2022-02-15 20:01:41 +0100

    router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 function App() {
   return (
-    <div>
-      <p>Hello Task App!</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
